99063d4e1c696d892d756f2d2241119d06ccefd4
GauravNagose1299 <bloggingplay@gmail.com>
2022-09-15 08:31:23 +0530

    first commit for report_card.js file

---
+++ report_card.js
@@ -0,0 +1,29 @@
+console.log("**********", "Report Card", "*********");
+
+let student_name= "Student Name: Gaurav Nagose";
+let college_name= "College Name: DG College of Science, Wardha";
+let grade= "Grade: A+";
+let class_name = "Class: 12th";
+let section = "Section: Science";
+let rollno= "Roll No: J141299";
+let math= "Math:";
+let math_mark= 80;
+let physics= "Physics:";
+let chemistry_mark= 81;
+let chemistry= "Chemistry:";
+let physics_mark= 78;
+let total_mark_student= math_mark+chemistry_mark+physics_mark;
+let total_mark= "Total Mark:";
+
+console.log(student_name);
+console.log(college_name);
+console.log(grade);
+console.log(class_name);
+console.log(section);
+console.log(rollno);
+console.log(math, math_mark);
+console.log(chemistry, chemistry_mark);
+console.log(physics, physics_mark);
+console.log(total_mark, total_mark_student);
+
+console.log("*******************");
